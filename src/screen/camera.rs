6d9070bc97c5b65092389605bf23fc23076adb76
Panadero1 <jacob.anderson.wa.us@gmail.com>
2021-09-12 21:11:53 -0700

    camera changes

--- src/screen/camera.rs
+++ src/screen/camera.rs
@@ -12,4 +12,7 @@ impl Camera {
     pub fn moove(&mut self, change_pos: (f32, f32)) {
         self.pos += (change_pos.0, change_pos.1).into();
     }
+    pub fn game_to_pix(&self, game_pos: GamePos) -> (f32, f32) {
+        todo!();
+    }
 }
